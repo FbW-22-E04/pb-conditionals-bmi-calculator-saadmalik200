ea4086fd9dd29bce0d3be966fed4e890f8cce21a
Saad Malik <saad_malik200@hotmail.com>
2022-07-11 16:01:54 +0200

    ex 2

--- solution.js
+++ solution.js
@@ -1,18 +1,30 @@
 //Q1 Store Mark's and John's mass and height in variables.
+const markHeight = 1.65;
+const markMass = 72.5;
+const johnHeight = 1.55;
+const johnMass = 65;
 
+//Q2 Calculate both their BMIs and store their BMIs in variables.
 
+const markBMI = markMass / (markHeight * markHeight);
+const johnBMI = johnMass / (johnHeight * johnHeight);
 
-//Q2 Calculate both their BMIs and store their BMIs in variables. 
-
-
-
+console.log(markBMI);
+console.log(johnBMI);
 //Q3 Create a boolean variable containing information about whether Mark has a higher BMI than John.
+const markMoreBeefCake = markBMI > johnBMI;
+console.log(markMoreBeefCake);
 
+//Q4 Print a string to the console containing the variable from step 3 using string interpolation. (e.g. "Is Mark's BMI higher than John's? Why yes, it's [boolean], it is").
 
+console.log(
+  `Q4: Is Mark's BMI higher than John's? Why yes, it's ${markMoreBeefCake}, it is`
+);
 
-//Q4 Print a string to the console containing the variable from step 3 using string interpolation. (e.g. "Is Mark's BMI higher than John's? Why yes, it's [boolean], it is"). 
+//Q5 Create an *if* statement which prints the name and BMI of the person with the highest BMI.
 
-
-
-//Q5 Create an *if* statement which prints the name and BMI of the person with the highest BMI. 
-
+if (markBMI > johnBMI) {
+  console.log(markBMI.toFixed(2));
+} else {
+  console.log(johnBMI.toFixed(2));
+}
